6f5c8c2c400ba10bc71be5258cbd0313dfd496d2
Yaroslav4tez <yaroslav4tez@gmail.com>
2025-06-19 08:50:27 +0200

    fixed valid

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 let userNumbers = prompt("your three numbers");
-
-let one = userNumbers[0]; 
-let too = userNumbers[1];
-let three = userNumbers[2];
-
-if (!userNumbers || isNaN(+userNumbers) || userNumbers.length !== 3 || userNumbers === null) {
+userNumbers = userNumbers.trim(userNumbers);
+if (!userNumbers || isNaN(+userNumbers) || userNumbers.length !== 3 ||
+     userNumbers === null || userNumbers.startsWith("0") || !/^\d+$/.test(userNumbers)) {
     console.log("pleas only numbers or only three numbers");
-} else if (one === too || one === three || too === one || too === three || three === one || three === too ) {
-    console.log("we have same numbers");
-}
-else {
-    console.log("diferents numbers");
+} else {
+    const one = userNumbers[0]; 
+    const too = userNumbers[1];
+    const three = userNumbers[2];
+    
+    if (one === too && too === three) {
+        console.log("всі числа однакові");
+    } else if (one === too || one === three || too === three){
+        console.log("присутні однакові числа");
+    } else {
+    console.log("різні цифри");
+    }
 }
